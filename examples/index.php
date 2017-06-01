33432dca45c35f32acea376c0a661d541a72fb9a
Jonah George <jonah.george@me.com>
2017-06-01 05:35:05 -0700

    Allow remote data sources

--- examples/index.php
+++ examples/index.php
@@ -1,14 +1,22 @@
-<?php require_once __DIR__ . "/../chartkick.php"; ?>
+<?php require_once __DIR__ . "/../src/Chartkick.php"; ?>
 
 <html>
     <head>
+        <title>Chartkick</title>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/chartkick/2.2.1/chartkick.min.js"></script>
         <script src="https://www.gstatic.com/charts/loader.js"></script>
     </head>
     <body>
-        <?php echo JonahGeorge\Chartkick\Chartkick::lineChart([
+        <?= JonahGeorge\Chartkick::lineChart([
             "2013-02-10 00:00:00 -0800" => 11,
             "2013-02-11 00:00:00 -0800" => 6,
         ]); ?>
+
+        <?= JonahGeorge\Chartkick::lineChart("http://echo.jsontest.com/Thu%20Jun%2001%202017%2000:00:00%20GMT+0000%20(UTC)/2/Fri%20Jun%2002%202017%2000:00:00%20GMT+0000%20(UTC)/3"); ?>
+
+        <?= JonahGeorge\Chartkick::pieChart([
+          "Football" => 10, 
+          "Basketball" => 5]
+        ) ?>
     </body>
 </html>
--- src/Chartkick.php
+++ src/Chartkick.php
@@ -1,70 +1,72 @@
 <?php
 
-namespace JonahGeorge\Chartkick;
+namespace JonahGeorge;
 
 class Chartkick
 {
-    protected $options = [];
-    protected static $chartkickChartId = 0;
+  protected $options = [];
+  protected static $chartId = 0;
 
-    public static function lineChart($dataSource, array $options = [])
-    {
-        return self::chartkickChart('LineChart', $dataSource, $options);
+  public static function lineChart($dataSource, array $options = [])
+  {
+    return self::genericChart('LineChart', $dataSource, $options);
+  }
+
+  public static function pieChart($dataSource, array $options = [])
+  {
+    return self::genericChart('PieChart', $dataSource, $options);
+  }
+
+  public static function columnChart($dataSource, array $options = [])
+  {
+    return self::genericChart('ColumnChart', $dataSource, $options);
+  }
+
+  public static function barChart($dataSource, array $options = [])
+  {
+    return self::genericChart('BarChart', $dataSource, $options);
+  }
+
+  public static function areaChart($dataSource, array $options = [])
+  {
+    return self::genericChart('AreaChart', $dataSource, $options);
+  }
+
+  public static function scatterChart($dataSource, array $options = [])
+  {
+    return self::genericChart('ScatterChart', $dataSource, $options);
+  }
+
+  public static function geoChart($dataSource, array $options = [])
+  {
+    return self::genericChart('GeoChart', $dataSource, $options);
+  }
+
+  private static function genericChart($chartType, $dataSource, array $options = [])
+  {
+    // $options = chartkick_deep_merge(Chartkick.options, options);
+    $elementId = $options["id"] ?? "chart-".(self::$chartId += 1);
+    $height = $options["height"] ?? "300px";
+    // $width = $options.delete(:width) || "100%";
+    // $defer = !!$options.delete(:defer);
+
+    // # content_for: nil must override default
+    // $content_for = $options.key?(:content_for) ? $options.delete(:content_for) : Chartkick.content_for;
+    // $nonce = $options.key?(:nonce) ? " nonce=\"#{ERB::Util.html_escape($options.delete(:nonce))}\"" : nil;
+    // $html = ($options.delete(:html) || %(DIV)) % {id: ERB::Util.html_escape(element_id), height: ERB::Util.html_escape(height), width: ERB::Util.html_escape(width)};
+
+    if (gettype($dataSource) == "string") {
+      $encodedData = '"' . $dataSource . '"';
+    } else {
+      $encodedData = json_encode($dataSource);
     }
 
-    public static function pieChart($dataSource, array $options = [])
-    {
-        return self::chartkickChart('PieChart', $dataSource, $options);
-    }
+    $str = sprintf(self::DIV, $elementId, $height, $height);
+    $str .= sprintf(self::SCRIPT, $chartType, $elementId, $encodedData);
 
-    public static function columnChart($dataSource, array $options = [])
-    {
-        return self::chartkickChart('ColumnChart', $dataSource, $options);
-    }
+    return $str;
+  }
 
-    public static function barChart($dataSource, array $options = [])
-    {
-        return self::chartkickChart('BarChart', $dataSource, $options);
-    }
-
-    public static function areaChart($dataSource, array $options = [])
-    {
-        return self::chartkickChart('AreaChart', $dataSource, $options);
-    }
-
-    public static function scatterChart($dataSource, array $options = [])
-    {
-        return self::chartkickChart('ScatterChart', $dataSource, $options);
-    }
-
-    public static function geoChart($dataSource, array $options = [])
-    {
-        return self::chartkickChart('GeoChart', $dataSource, $options);
-    }
-
-    private static function chartkickChart($chartType, $dataSource, array $options = [])
-    {
-//        $options = chartkick_deep_merge(Chartkick.options, options);
-//        $element_id = $options.delete(:id) || "chart-#{@chartkick_chart_id += 1}";
-//        $height = $options.delete(:height) || "300px";
-//        $width = $options.delete(:width) || "100%";
-//        $defer = !!$options.delete(:defer);
-
-//        # content_for: nil must override default
-//        $content_for = $options.key?(:content_for) ? $options.delete(:content_for) : Chartkick.content_for;
-//        $nonce = $options.key?(:nonce) ? " nonce=\"#{ERB::Util.html_escape($options.delete(:nonce))}\"" : nil;
-//        $html = ($options.delete(:html) || %(DIV)) % {id: ERB::Util.html_escape(element_id), height: ERB::Util.html_escape(height), width: ERB::Util.html_escape(width)};
-
-        $chartId = static::$chartkickChartId += 1;
-        $encodedData = json_encode($dataSource);
-
-        $str = sprintf(self::DIV, $chartId);
-        $str .= sprintf(self::SCRIPT, $chartType, $chartId, $encodedData);
-
-        return $str;
-    }
-
-    const DIV = "<div id='chart-%d' style='height: 300px'></div>";
-//<div id="%{id}" style="height: %{height}; width: %{width}; text-align: center; color: #999; line-height: %{height}; font-size: 14px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif;">Loading...</div>
-    const SCRIPT = "<script type='application/javascript'>new Chartkick.%s('chart-%d', %s);</script>";
+  const DIV = "<div id=\"%s\" style=\"height: %s; text-align: center; color: #999; line-height: %s; font-size: 14px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif;\">Loading...</div>";
+  const SCRIPT = "<script type='application/javascript'>new Chartkick.%s('%s', %s);</script>";
 }
